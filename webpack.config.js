529aee26591d9b222d5f4d3758e0a152fff624ca
yuta19851203 <biohazardlevelmouth@icloud.com>
2023-03-20 19:26:19 +0900

    Generate HTML through pug

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,20 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name: './image/[name].[ext]'
+              name: 'images/[name].[ext]'
             },
           },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+          }
         ],
       },
     ],
@@ -41,8 +52,13 @@ module.exports = {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html'
+      template: './src/templates/index.pug',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html'
     }),
     new CleanWebpackPlugin(),
-  ]
+  ],
 }
