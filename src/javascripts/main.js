551c40b12f97e2b621e82ae3c13d409147ee630b
yuta19851203 <biohazardlevelmouth@icloud.com>
2023-03-20 18:09:56 +0900

    Improve file structure

---
+++ src/javascripts/main.js
@@ -0,0 +1,5 @@
+import my from './my'
+import '../stylesheets/main.css'
+
+console.log('webpack')
+my()
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    filename: './javascripts/main.js',
   },
   module: {
     rules: [
@@ -23,9 +25,12 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/main.css',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html'
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
